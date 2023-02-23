db877325b5745fe319260a197e35ea474ea50b0b
Lonely Glitch <lonelyglitch25@gmail.com>
2023-02-23 19:49:22 +0330

    initial commit

---
+++ ergb.py
@@ -0,0 +1,50 @@
+import argparse
+from urllib.request import urlopen, Request
+import json
+import colorama
+from colorama import Back
+argparse = argparse.ArgumentParser()
+argparse.add_argument("-r", "--rial", action="store_true", help="shows the values in IRR")
+argparse.add_argument("-d", "--difference", action="store_true", help="shows the difference point")
+args = argparse.parse_args()
+colorama.init(autoreset=True)
+try:
+    req = Request(
+        url = "https://www.iranintl.com/api/finance?locale=en",
+        headers={'User-Agent': 'Mozilla/5.0'}
+    )
+    response = urlopen(req)
+except:
+    print(Back.RED + "Network Error!")
+    exit(1)
+data_json = json.loads(response.read())
+lookFor = ["USD", "EUR", "GBP", "Emami Coin"]
+names = {"USD": "💵 USD", "EUR": "💶 EUR", "GBP": "💷 GBP", "Emami Coin": "🪙 EGC"}
+def convert_to_irr(inp):
+    inp = inp.replace('٬', '')
+    number = int(inp)
+    number = number * 10
+    rial_val = "{:,}".format(number)
+    return rial_val
+def print_value(val):
+    if args.rial is True:
+        return convert_to_irr(val) + " IRR "
+    else: 
+        return val + " TMN "
+def show_diff(val):
+    if args.difference is True:
+        if args.rial is True:
+            return str(val * 10) + " TMN "
+        else: 
+            return str(val) + " TMN "
+    return ""
+def print_data(info):
+    if info["move"] == "up":
+        print(Back.GREEN + names[info["title"]] + " = " + print_value(info["value"]) + "⬆️ " + show_diff(info["differenceP"]))
+    elif info["move"] == "static":
+        print(Back.YELLOW + names[info["title"]] + " = " + print_value(info["value"]) + "⏸️ " + show_diff(info["differenceP"]))
+    elif info["move"] == "down":
+        print(Back.RED + names[info["title"]] + " = " + print_value(info["value"]) + "⬇️ " + show_diff(info["differenceP"]))
+for item in data_json["items"]:
+    if item["title"] in lookFor:
+        print_data(item)
---
+++ readme.md
@@ -0,0 +1,17 @@
+# **Exchange Rate Goes Brr!**
+
+This is a simple script that fetches latest exchange rates for Iranian Currency and gold coins in Iranian market using ![iranintl logo](https://raw.githubusercontent.com/lonelyglitch/ergb/main/src/iranintl-logo.png) IranIntl's API and conveniently displays them in the terminal.
+
+## 🔧 Dependencies
+
+This script depends on following modules:
+
+```
+argparse
+urllib
+json 
+colorama
+```
+
+![terminal output](https://raw.githubusercontent.com/lonelyglitch/ergb/main/src/sample.png)
+
